09d65adc32215d44bdab85212747ec004775b2e3
Joan Teixidó <joan.teixido@bismart.com>
2025-01-03 12:41:00 +0100

    landing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { PlayerComponent } from './player/player.component';
+import { LandingComponent } from './landing/landing.component';
 
 export const routes: Routes = [
-    { path: 'player/:id', component: PlayerComponent   }
+    { path: 'player/:id', component: PlayerComponent   },
+    { path: '', component: LandingComponent}
 ];
