2728055c5184ed60c9123c78bd3d695c5f546014
Joan Teixidó <joan.teixido@bismart.com>
2025-01-03 10:33:24 +0100

    initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PlayerComponent } from './player/player.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'player', component: PlayerComponent }
+];
